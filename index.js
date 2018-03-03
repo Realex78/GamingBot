0c16bafecff84c706b8831d23e819a8e5f3e63ad
Realex78 <36749154+Realex78@users.noreply.github.com>
2018-03-03 11:43:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js");
 const YTDL = require("ytdl-core");
 
-const TOKEN = proccess.env.BOT_TOKEN;
+const TOKEN = process.env.BOT_TOKEN;
 const PREFIX = "/"
 
 var bot = new Discord.Client();
